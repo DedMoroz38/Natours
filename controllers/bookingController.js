53c42fb7970992d6b8c3d5cfb9d1b262d9cd06c9
Your Namemaksimovegor401@gmail.com <you@example.com>
2022-06-13 20:05:32 +0100

    Updated stripe functionality

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -48,11 +48,13 @@ exports.getCheckoutSession = catchAsync( async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  // const price = session.display_items[0].amount / 100;
+  const price = 100;
   await Booking.create({ tour, user, price });
 };9
 
 exports.webhookCheckout = (req, res, next) => {
+  console.log("Here");
   const signature = req.headers['stripe-signature'];
 
   let event;
